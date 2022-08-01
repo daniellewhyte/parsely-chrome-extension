9225474cda38879e99a33fd86e60f59491b3acbf
Danielle <dwhyte1226@gmail.com>
2022-07-31 21:01:45 -0500

    created file for apicalls

---
+++ src/apiCalls.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const baseURL = "https://parsely-backend.herokuapp.com";
+const HEADERS = {
+  headers: {
+    "Content-Type": "application/vnd.api+json",
+  },
+};
+
+const createRequest = (obj) => {
+  return (
+    {
+      data: {
+        type: 'Recipe', 
+        attributes : {
+          url: obj
+        }
+      }
+    }
+  )
+}
+
+export const postRecipeAsync = (url) => {
+  return axios
+    .post(
+      `${baseURL}/recipes/`,
+      createRequest(url),
+      HEADERS
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log("Error creating recipe");
+    });
+};
+
+export const getAllRecipesAsync = () => {
+  return axios
+    .get(
+      `${baseURL}/titles/`, HEADERS
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log("Error getting all recipes")
+    });
+};
