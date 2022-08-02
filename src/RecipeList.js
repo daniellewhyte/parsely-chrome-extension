5cb2309578df8cc64732f19d0be5ee38648f026c
Danielle <dwhyte1226@gmail.com>
2022-08-02 10:41:14 -0500

    move state into RecipeList

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,20 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { getAllRecipesAsync, getFullRecipeAsync } from "./apiCalls";
 
-const RecipeList = (props) => {
-  return(
-    props.recipeData.map((recipe) => {
-      const handleClick = () => {
-        props.onButtonClick(recipe.id);
-      }
-      return <button key={recipe.id} onClick={handleClick}>
-        {recipe.title}
+const RecipeList = () => {
+  const [recipeData, setRecipeData] = useState([]);
+  const [fullRecipe, setFullRecipe] = useState({
+    title: "",
+    instructions: "",
+    ingredients: "",
+  });
+
+  const getAllRecipes = () => {
+    getAllRecipesAsync()
+      .then((recipes) => {
+        setRecipeData(recipes);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(getAllRecipes, [recipeData]);
+
+  const showRecipe = (id) => {
+    getFullRecipeAsync(id)
+      .then((recipe) => {
+        setFullRecipe(recipe);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const recipeButtons = recipeData.map((recipe) => {
+    const handleClick = () => {
+      showRecipe(recipe.id);
+    };
+    return (
+      <button key={recipe.id} onClick={handleClick}>
+        {recipe.title}{" "}
       </button>
-    })
-  )
-}
+    );
+  });
 
-RecipeList.PropTypes = {
-  recipeData: PropTypes.arrayOf(PropTypes.object),
-}
-export default RecipeList
+  return (
+    <div>
+      <section>
+        {recipeButtons}
+      </section>
+      <section>
+        <h2>{fullRecipe.title}</h2>
+        <p>{fullRecipe.ingredients}</p>
+        <p>{fullRecipe.instructions}</p>
+      </section>
+
+    </div>
+  )
+};
+
+// RecipeList.PropTypes = {
+//   recipeData: PropTypes.arrayOf(PropTypes.object),
+// };
+export default RecipeList;
