93f172fb10ed360f158bd1ea73671734a6b6193f
Danielle <dwhyte1226@gmail.com>
2022-08-02 13:58:54 -0500

    unnested routes and use hashrouter

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AppRoutes from './Routes';
 import RecipeList from './RecipeList'
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element ={<App/>}>
-          <Route path ="/my-recipes" element={<RecipeList/>}>
-          </Route>
+        </Route>
+        <Route path ="/my-recipes" element={<RecipeList/>}>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
