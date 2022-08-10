9326fb43e36fe8af6f4fcb931822deca32b4c66e
Danielle <dwhyte1226@gmail.com>
2022-08-10 18:28:44 -0500

    created function to delete recipe

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,29 +8,23 @@ const HEADERS = {
 };
 
 const createRequest = (obj) => {
-  return (
-    {
-      data: {
-        type: 'Recipe', 
-        attributes : {
-          url: obj
-        }
-      }
-    }
-  )
-}
+  return {
+    data: {
+      type: "Recipe",
+      attributes: {
+        url: obj,
+      },
+    },
+  };
+};
 
 const createObject = (responseData) => {
-  return {...responseData.attributes, id: responseData.id};
+  return { ...responseData.attributes, id: responseData.id };
 };
 
 export const postRecipeAsync = (url) => {
   return axios
-    .post(
-      `${baseURL}/recipes/`,
-      createRequest(url),
-      HEADERS
-    )
+    .post(`${baseURL}/recipes/`, createRequest(url), HEADERS)
     .then((response) => {
       return response.data;
     })
@@ -41,9 +35,7 @@ export const postRecipeAsync = (url) => {
 
 export const getAllRecipesAsync = () => {
   return axios
-    .get(
-      `${baseURL}/titles/`, HEADERS
-    )
+    .get(`${baseURL}/titles/`, HEADERS)
     .then((response) => {
       return response.data.data.map(createObject);
     })
@@ -54,13 +46,22 @@ export const getAllRecipesAsync = () => {
 
 export const getFullRecipeAsync = (id) => {
   return axios
-    .get(
-      `${baseURL}/recipes/${id}`, HEADERS
-    )
+    .get(`${baseURL}/recipes/${id}`, HEADERS)
     .then((response) => {
-      return createObject(response.data.data)
+      return createObject(response.data.data);
     })
     .catch((err) => {
       console.log(err.message);
     });
-}
+};
+
+export const deleteRecipeAsync = (id) => {
+  return axios
+    .delete(`${baseURL}/recipes/${id}`, HEADERS)
+    .then(() => {
+      console.log("Recipe deleted");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
